1239c88feeb62de2227faf30a8d3048ab4f3dea9
Andy T. <contact@vietcoop.com>
2012-10-16 20:15:11 +0700

    Auto loading.

---
+++ vc_example/lib/Lazy.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @file lib/Lazy.php
+ */
+
+class VcExampleLazy {
+  public function hi() {
+    return 'From ' .__CLASS__;
+  }
+}
---
+++ vc_example/lib/VcExample.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @file VcExample.php
+ */
+
+class VcExample {
+  public function hello() {
+    return 'From ' .__CLASS__;
+  }
+}
