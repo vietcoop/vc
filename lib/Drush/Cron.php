69e67aef5af1a8986805b6c5b6792006536a5779
Andy Truong <andy@go1.com.au>
2013-04-29 04:23:57 +0700

    Allow run specific cron job.

--- lib/Drush/Cron.php
+++ lib/Drush/Cron.php
@@ -4,12 +4,14 @@ class Vc_Drush_Cron {
   public static function callback() {
     $args = drush_get_arguments();
     array_shift($args);
+
     if (empty($args)) {
       return self::runAll();
     }
 
     $module = array_shift($args);
-    return self::runModuleCrons($module);
+    $cron_job = array_shift($args);
+    return self::runModuleCrons($module, $cron_job);
   }
 
   public static function runAll() {
@@ -17,37 +19,48 @@ class Vc_Drush_Cron {
       self::runModuleCrons($module);
     }
   }
-
-  protected static function runModuleCrons($module) {
+  
+  protected static function runModuleCrons($module, $cron_job = NULL) {    
     $file = drupal_get_path('module', $module);
     $file = DRUPAL_ROOT . '/' . $file . "/config/{$module}.cron.yaml";
     if (file_exists($file)) {
-      drush_print_r(" › Running cron jobs in {$module} module");
+      function_exists('drush_print_r') && drush_print_r(" › Running cron jobs in {$module} module");
 
+      // Get job infos
       if (!$jobs = yaml_parse_file($file)) continue;
-      foreach ($jobs as $name => $job) {
-        drush_print_r("   » Running {$module} › {$name}");
 
-        if (!empty($job['lock'])) {
-          $lock_name = md5("vc_cron_{$module}_{$name}");
-          $lock_timeout = isset($job['lock timeout']) ? $job['lock timeout'] : 60;
-          if (!lock_acquire($lock_name, $lock_timeout)) {
-            drush_print_r("      | An other process is running…");
-            return;
-          }
-        }
-
-        if (is_callable($job['callback'])) {
-          call_user_func($job['callback']);
-        }
-        else {
-          drush_print_r("      | Callback is not callable: {$job['callback']}");
-        }
-
-        if ($job['lock']) {
-          lock_release($lock_name);
-        }
+      // Only run one job!
+      if (!empty($cron_job) && isset($jobs[$cron_job])) {
+        $jobs = array($cron_job => $jobs[$cron_job]);
       }
+
+      // Loop to execute the jobs
+      foreach ($jobs as $job_name => $info) {
+        function_exists('drush_print_r') && drush_print_r("   » Running {$module} › {$job_name}");
+        self::runModuleCron($module, $job_name, $info);
+      }
+    }
+  }
+
+  protected static function runModuleCron($module, $job_name, $job_info) {
+    if (!empty($job_info['lock'])) {
+      $lock_name    = md5("vc_cron_{$module}_{$job_name}");
+      $lock_timeout = isset($job_info['lock timeout']) ? $job_info['lock timeout'] : 60;
+      if (!lock_acquire($lock_name, $lock_timeout)) {
+        function_exists('drush_print_r') && drush_print_r("      | An other process is running…");
+        return;
+      }
+    }
+
+    if (is_callable($job_info['callback'])) {
+      call_user_func($job_info['callback']);
+    }
+    else {
+      drush_print_r("      | Callback is not callable: {$job_info['callback']}");
+    }
+
+    if ($job_info['lock']) {
+      lock_release($lock_name);
     }
   }
 }
